539caaa31a98f4e40a6b9cb3a82b3e83b074baf1
Andrew <kobe73er@gmail.com>
2021-04-04 22:57:20 +0800

    init

---
+++ scripts/stop-app.sh
@@ -0,0 +1,9 @@
+set -x
+PID=$(lsof -t -i:3001)
+if [ -z "${PID}" ]; then
+    echo "no node app running..."
+  else
+    echo "stop app running on port 3001 with PID ${PID}"
+    kill -9 ${PID}
+fi
+set +x
